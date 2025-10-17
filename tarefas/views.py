ebb182b1bbbfd644a3b300b433bf070ea5afa452
Rian Aguiar Santos <aguiarsantosrian@gmail.com>
2025-10-17 01:19:16 -0300

    aa

--- tarefas/views.py
+++ tarefas/views.py
@@ -2,20 +2,68 @@ from django.shortcuts import render , redirect, get_object_or_404
 from .models import tarefaModels
 from .forms import tarefaForm
 from django.http import HttpRequest
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
-def tarefas_index(request):
-    return render(request,'index.html')
+'''-------------------------------------------------------------------------------------------------'''
 
 def tarefas_cadastro(request):
-    return render(request,'cadastro.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        senha = request.POST['senha']
+        confirmar = request.POST['confirmar']
+        if senha != confirmar:
+            messages.error(request, 'As senhas não coincidem.')
+            return redirect('tarefas:cadastro')
 
+        if User.objects.filter(email=email).exists():
+            messages.error(request, 'E-mail já cadastrado.')
+            return redirect('tarefas:cadastro')
+        user = User.objects.create_user(username=username, email=email, password=senha)
+        user.save()
+        messages.success(request, 'Conta criada com sucesso! Faça login.')
+        return redirect('tarefas:index')
+    return render(request, 'cadastro.html')
+
+'''-------------------------------------------------------------------------------------------------'''
+
+def tarefas_index(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        senha = request.POST['senha']
+        try:
+            user = User.objects.get(email=email)
+            user = authenticate(request, username=user.username, password=senha)
+            if user is not None:
+                login(request, user)
+                return redirect('tarefas:home')
+            else:
+                messages.error(request, 'Email ou senha inválidos.')
+        except User.DoesNotExist:
+            messages.error(request, 'Usuário não encontrado.')
+    return render(request, 'index.html')
+
+'''-------------------------------------------------------------------------------------------------'''
+
+def logout_view(request):
+    logout(request)
+    return redirect('tarefas:login')
+
+'''-------------------------------------------------------------------------------------------------'''
+
+@login_required(login_url='tarefas:index')
 def tarefas_home(request):
     context={
         'tarefas':tarefaModels.objects.all()
     }
     return render(request,'home.html',context)
 
+'''-------------------------------------------------------------------------------------------------'''
 
+@login_required(login_url='tarefas:index')
 def tarefas_adicionar(request:HttpRequest):
     if request.method == 'POST':
         formulario = tarefaForm(request.POST)
@@ -25,11 +73,17 @@ def tarefas_adicionar(request:HttpRequest):
     context={'form' : tarefaForm } 
     return render(request,'adicionar.html', context)
 
+'''-------------------------------------------------------------------------------------------------'''
+
+@login_required(login_url='tarefas:index')
 def tarefas_remover(request:HttpRequest,id):
     item = get_object_or_404(tarefaModels, id=id)
     item.delete()
     return redirect('tarefas:home')
 
+'''-------------------------------------------------------------------------------------------------'''
+
+@login_required(login_url='tarefas:index')
 def tarefas_editar(request:HttpRequest,id):
     item = get_object_or_404(tarefaModels, id=id)
     if request.method == 'POST':
