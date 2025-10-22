8410b0fd5958e4019c8e57d67c29954a9bc95d95
Rian Aguiar Santos <aguiarsantosrian@gmail.com>
2025-10-22 18:35:30 -0300

    a

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -5,15 +5,15 @@ app_name = 'tarefas'
 
 urlpatterns = [
     path('', views.tarefas_index, name='index'),
-    path('login/', views.tarefas_login, name='login'),
     path('cadastro/', views.tarefas_cadastro, name='cadastro'),
+    path('login/', views.tarefas_login, name='login'),
     path('logout/', views.tarefas_logout, name='logout'),
     path('home/', views.tarefas_home, name='home'),
     path('adicionar/', views.tarefas_adicionar, name='adicionar'),
     path('editar/<int:id>/', views.tarefas_editar, name='editar'),
     path('remover/<int:id>/', views.tarefas_remover, name='remover'),
-    path('removerUsuario/<int:id>/', views.tarefas_removerUsuario, name='removerUsuario'),
     path('adm/', views.tarefas_adm, name='adm'),
+    path('removerUsuario/<int:id>/', views.tarefas_removerUsuario, name='removerUsuario'),
 
 
 ]
